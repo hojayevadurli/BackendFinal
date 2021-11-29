42a7082b2d1313c9b5313a78aa6b6323f58a649a
hojayevadurli@gmail.com <hojayevadurli@gmail.com>
2021-11-29 10:44:11 -0700

    First Commit

---
+++ ScoreCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final
+{
+    public class ScoreCalculator
+    {
+        public int AddScores(IEnumerable<int> scores)
+        {
+            var sum = 0;
+            foreach(var score in scores)
+            {
+                sum += score;
+
+            }
+            return sum;
+        }
+    }
+}
