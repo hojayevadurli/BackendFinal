b3371e02a107fc88a164447ece63776faa2599c7
hojayevadurli@gmail.com <hojayevadurli@gmail.com>
2021-12-07 12:35:28 -0700

    Added topic details page but button is not working

---
+++ Migrations/20211207192621_ChangedPostsTable.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Final.Migrations
+{
+    public partial class ChangedPostsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Topics_TopicID",
+                table: "Posts");
+
+            migrationBuilder.RenameColumn(
+                name: "TopicID",
+                table: "Posts",
+                newName: "TopicId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Posts_TopicID",
+                table: "Posts",
+                newName: "IX_Posts_TopicId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TopicId",
+                table: "Posts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Body",
+                table: "Posts",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastEditedBy",
+                table: "Posts",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastEditedOn",
+                table: "Posts",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Published",
+                table: "Posts",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Topics_TopicId",
+                table: "Posts",
+                column: "TopicId",
+                principalTable: "Topics",
+                principalColumn: "TopicID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Topics_TopicId",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Body",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "LastEditedBy",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "LastEditedOn",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Published",
+                table: "Posts");
+
+            migrationBuilder.RenameColumn(
+                name: "TopicId",
+                table: "Posts",
+                newName: "TopicID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Posts_TopicId",
+                table: "Posts",
+                newName: "IX_Posts_TopicID");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TopicID",
+                table: "Posts",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Topics_TopicID",
+                table: "Posts",
+                column: "TopicID",
+                principalTable: "Topics",
+                principalColumn: "TopicID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
